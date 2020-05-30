46002b51d748a188a4bcdce469d91e2dd1639fa9
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-30 21:15:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(array)
 array.split(" ").each {|x| print x.reverse!, " " }
 end
 reverse_each_word("Hello there, and how are you?")
+
+# use .collect
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
+reverse_each_word("Hello there, and how are you?")
